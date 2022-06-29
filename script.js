31a1f0ff0646b03ada2a9c0e8351aeb825d8419e
Devienne Marc <mdevienne1@gmail.com>
2022-06-29 17:15:22 +0200

    Fin EP 59 MERGING TWO ARRAYS

--- script.js
+++ script.js
@@ -34,7 +34,32 @@ const calcTempAmplitude = function (temps) {
     if (curTemp < min) min = curTemp;
   }
   console.log(max, min);
+  return max - min;
 };
-calcTempAmplitude([temperatures]);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
 
+// PROBLEM 2:
 // MERGE TWO ARRAYS
+
+// 1) Comprendre le problème
+// - Avec 2 tableaux, devons nous utiliser 2 fois la fonction? Non, fusionnez les deux tableaux
+
+// Breaking up into sub-problems
+// - Merge 2 ARRAYS
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
